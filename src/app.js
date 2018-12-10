5ee6c8655587ce9d52f248c95cc8d9ecf677ab23
Brett Hurst <bretthurst@outlook.com>
2018-12-09 17:00:44 -0800

    WIP deploy to production

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+const appConfig = require("./config/main-config");
 const routeConfig = require("./config/route-config.js");
 
+appConfig.init();
 routeConfig.init(app);
 
 module.exports = app;
