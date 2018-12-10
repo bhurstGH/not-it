2c80ffdcce28c7bf89e15d2a6132c00a1a097c62
Brett Hurst <bretthurst@outlook.com>
2018-12-09 17:20:59 -0800

    Small typo fix.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const appConfig = require("./config/main-config");
+const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
 
 appConfig.init();
