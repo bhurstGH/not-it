8247442c41d4c2b76191c0964bb1977f8d379307
Brett Hurst <bretthurst@outlook.com>
2018-12-11 15:28:26 -0800

    Completed Topic resource 2 work.

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -40,5 +40,118 @@ describe("routes : topics", () => {
         });
         
     });
-    
+
+    describe("GET /topics/new", () => {
+
+        it("should render a new topic form", done => {
+            request.get(`${base}new`, (err, res, body) => {
+                expect(err).toBeNull();
+                expect(body).toContain("New Topic");
+                done();
+            });
+        });
+        
+    });
+
+    describe("POST /topics/create", () => {
+        const options = {
+            url: `${base}create`,
+            form: {
+                title: "blink-182 songs",
+                description: "What's your favorite blink-182 song?"
+            }
+        };
+
+        it("should create a new topic and redirect", done => {
+
+            request.post(options,
+                (err, res, body) => {
+                    Topic.findOne({where: {title: "blink-182 songs"}})
+                    .then(topic => {
+                        expect(res.statusCode).toBe(303);
+                        expect(topic.title).toBe("blink-182 songs");
+                        expect(topic.description).toBe("What's your favorite blink-182 song?");
+                        done();
+                    }).catch(err => {
+                        console.log(err);
+                        done();
+                    });
+                }
+            );
+        });
+    });
+
+    describe("GET /topics/:id", () => {
+
+        it("should render a view with the selected topic", done => {
+            request.get(`${base}${this.topic.id}`, (err, res, body) => {
+                expect(err).toBeNull();
+                expect(body).toContain("JS Frameworks");
+                done();
+            });
+        });
+        
+    });
+
+    describe("POST /topics/:id/destroy", () => {
+        
+        it("should delete the topic with the associated ID", done => {
+            Topic.findAll()
+            .then(topics => {
+                const topicCountBeforeDelete = topics.length;
+                expect(topicCountBeforeDelete).toBe(1);
+                request.post(`${base}${this.topic.id}/destroy`, (err, res, body) => {
+                    Topic.findAll()
+                    .then(topics => {
+                        expect(err).toBeNull();
+                        expect(topics.length).toBe(topicCountBeforeDelete - 1);
+                        done();
+                    })
+                });
+            });
+        });
+        
+    });
+
+    describe("GET /topics/:id/edit", () => {
+
+        it("should render a view with an edit topic form", done => {
+            request.get(`${base}${this.topic.id}/edit`, (err, res, body) => {
+                expect(err).toBeNull();
+                expect(body).toContain("Edit Topic");
+                expect(body).toContain("JS Frameworks");
+                done();
+            });
+        });
+
+    });
+
+    describe("POST /topics/:id/update", () => {
+
+        it("should update the topic with the given values", done => {
+            const options = {
+                url: `${base}${this.topic.id}/update`,
+                form: {
+                    title: "JavaScript Frameworks",
+                    description: "There are a lot of them"
+                }
+            };
+
+            request.post(options,
+                (err, res, body) => {
+                
+                expect(err).toBeNull();
+
+                Topic.findOne({
+                    where: { id: this.topic.id }
+                })
+                .then(topic => {
+                    expect(topic.title).toBe("JavaScript Frameworks");
+                    done();
+                });
+            });
+        });
+        
+    });
+
 });
--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -9,5 +9,51 @@ module.exports = {
         }).catch(err => {
             callback(err);
         })
+    },
+    getTopic(id, callback) {
+        return Topic.findByPk(id)
+        .then(topic => {
+            callback(null, topic);
+        }).catch(err => {
+            callback(err);
+        })
+    },
+    addTopic(newTopic, callback){
+        return Topic.create({
+            title: newTopic.title,
+            description: newTopic.description
+        })
+        .then(topic => {
+            callback(null, topic);
+        }).catch(err => {
+            callback(err);
+        })
+    },
+    deleteTopic(id, callback) {
+        return Topic.destroy({
+            where: {id}
+        })
+        .then(topic => {
+            callback(null, topic);
+        }).catch(err => {
+            callback(err);
+        })
+    },
+    updateTopic(id, updatedTopic, callback) {
+        return Topic.findByPk(id)
+        .then(topic => {
+            if (!topic) {
+                return callback("Topic not found");
+            }
+
+            topic.update(updatedTopic, {
+                fields: Object.keys(updatedTopic)
+            })
+            .then(() => {
+                callback(null, topic);
+            }).catch(err => {
+                callback(err);
+            });
+        });
     }
 }
