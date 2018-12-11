4f136de4b229f85b229746dfd9b3be0268c9bb9b
Brett Hurst <bretthurst@outlook.com>
2018-12-11 12:09:12 -0800

    Completed topic resource part 1.

---
+++ src/db/models/banner.js
@@ -0,0 +1,24 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Banner = sequelize.define('Banner', {
+    source: DataTypes.STRING,
+    description: DataTypes.STRING,
+    topicId: {
+      type: DataTypes.INTEGER,
+      onDelete: "CASCADE",
+      references: {
+        model: "Topics",
+        key: "id",
+        as: "topicId",
+      }
+    }
+  }, {});
+  Banner.associate = function(models) {
+    // associations can be defined here
+    Banner.belongsTo(models.Topic, {
+      foreignKey: "topicId",
+      onDelete: "CASCADE",
+    });
+  };
+  return Banner;
+};
