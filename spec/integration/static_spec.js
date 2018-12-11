76ecba5e812acbe89d627964bcf9e980381dcdc5
Brett Hurst <bretthurst@outlook.com>
2018-12-11 09:03:11 -0800

    Completed routing checkpoint work.

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -6,10 +6,10 @@ describe("routes : static", () => {
 
     describe("GET /", () => {
 
-        it("should return status code 200", done => {
+        it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
             request.get(base, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
-                done();
+                expect(body).toContain("Welcome to Bloccit");
             });
         });
 
