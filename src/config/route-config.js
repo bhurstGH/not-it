c9c8b2c07c777f7485f67fa20f7ff2783da9d638
Brett Hurst <bretthurst@outlook.com>
2018-12-15 13:35:45 -0800

    Completed checkpoint work for Post 2.

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,8 +2,10 @@ module.exports = {
     init(app) {
         const staticRoutes = require("../routes/static");
         const topicRoutes = require("../routes/topics");
+        const postRoutes = require("../routes/posts");
         
         app.use(staticRoutes);
         app.use(topicRoutes);
+        app.use(postRoutes);
     }
 }
--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -1,4 +1,5 @@
 const Topic = require("./models").Topic;
+const Post = require("./models").Post;
 
 module.exports = {
 
@@ -11,7 +12,12 @@ module.exports = {
         })
     },
     getTopic(id, callback) {
-        return Topic.findByPk(id)
+        return Topic.findByPk(id, {
+            include: [{
+                model: Post,
+                as: "posts"
+            }]
+        })
         .then(topic => {
             callback(null, topic);
         }).catch(err => {
