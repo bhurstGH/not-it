a6a678edfbcfbc512a928c4937bfa757ff2c5c53
Brett Hurst <bretthurst@outlook.com>
2018-12-18 03:28:25 -0800

    Checkpoint User auth work completed.

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -3,9 +3,11 @@ module.exports = {
         const staticRoutes = require("../routes/static");
         const topicRoutes = require("../routes/topics");
         const postRoutes = require("../routes/posts");
+        const userRoutes = require("../routes/users");
         
         app.use(staticRoutes);
         app.use(topicRoutes);
         app.use(postRoutes);
+        app.use(userRoutes);
     }
 }
