5046990376f354ca389679d5b588b69d08c4d19e
Brett Hurst <bretthurst@outlook.com>
2018-12-22 20:02:47 -0800

    Implemented voting functionality.

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -5,6 +5,7 @@ module.exports = {
         const postRoutes = require("../routes/posts");
         const userRoutes = require("../routes/users");
         const commentRoutes = require("../routes/comments");
+        const voteRoutes = require("../routes/votes");
 
         if (process.env.NODE_ENV === "test") {
             const mockAuth = require("../../spec/support/mock-auth.js");
@@ -16,5 +17,6 @@ module.exports = {
         app.use(postRoutes);
         app.use(userRoutes);
         app.use(commentRoutes);
+        app.use(voteRoutes);
     }
 }
